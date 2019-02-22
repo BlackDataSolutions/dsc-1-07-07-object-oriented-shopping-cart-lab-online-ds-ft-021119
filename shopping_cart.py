ac00e5a75cb936c2bdf40a6c035d6e90035989f1
Alex Black <alexander.p.black@gmail.com>
2019-02-22 16:55:48 -0500

    lab done feelin good

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,58 @@
 class ShoppingCart:
     # write your code here
-    def __init__(self, emp_discount=None):
-      pass
+    def __init__(self, employee_discount=None):
+        self.total = 0
+        self.employee_discount = employee_discount
+        self.items = []
+     
+    
+    
     def add_item(self, name, price, quantity=1):
-       pass
+        self.name = name
+        self.calculated_price = price * quantity
+        self.quantity = quantity
+        
+        if self.quantity == 1:
+            self.items.append({"name": name, "price": price})
+        else:
+            for item in range(0, self.quantity):
+                self.items.append({"name": name, "price": price})
+                
+        self.total += self.calculated_price
+        return self.total
+
+    def show_item_list(self):
+        return self.items
+
+
     def mean_item_price(self):
-       pass
+        return round(self.total / len(self.items), 3)
+    
 
     def median_item_price(self):
-        pass
+        sorted_list = sorted(self.items, key= lambda cost: cost['price'])
+        price_list = [item['price'] for item in sorted_list]
+        length = len(price_list)
+        if (length % 2 == 0):
+            mid_upper = price_list[int(length / 2)]
+            mid_lower = price_list[int((length / 2) - 1)]
+            median = (mid_upper + mid_lower) / 2
+            return round(median, 3)
+        else:
+            median = price_list[int((length - 1)/2)]
+            return median
+        
 
     def apply_discount(self):
-       pass
+        if self.employee_discount:
+            discounted_total = (self.total * ( 1 - (self.employee_discount / 100)))
+            return discounted_total
+        else:
+            return "Sorry, there is no discount to apply to your cart :("
 
     def void_last_item(self):
-        pass
+        if self.items:
+            removed_item = self.items.pop()
+        else:
+            return "There are no items in your cart!"
+        self.total -= removed_item['price']
